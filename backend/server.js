6e478a74682edcbc3370b83c9cfa0a188eec2aea
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-05-20 22:50:14 +0530

    file aded

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
 
 // Loading env vars
 dotenv.config();
@@ -13,17 +13,21 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
 // Routes API
-app.use('/api/users', require('./routes/authRoutes'));
-app.use('/api/posts', require('./routes/postRoutes'));
-app.use('/api/generate', require('./routes/generateRoutes'));
+app.use("/api/users", require("./routes/authRoutes"));
+app.use("/api/posts", require("./routes/postRoutes"));
+app.use("/api/generate", require("./routes/generateRoutes"));
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// CRUCIAL FOR VERCEL: Export the app instance
+module.exports = app;
